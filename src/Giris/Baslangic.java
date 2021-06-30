489fa7a240d9827ae964744e2e6cc477867b3080
Erez <erezakdogan.ea@gmail.com>
2021-06-30 17:10:04 +0300

    odev added

---
+++ src/Giris/Baslangic.java
@@ -0,0 +1,37 @@
+package src.Giris;
+
+import java.util.Scanner;
+
+public class Baslangic {
+public static void main(String[] args) {
+    int mat, fiz, kim, biyo, tarih, turkce, muzik;
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Matematik Notunuzu Giriniz : ");
+    mat = input.nextInt();
+
+    System.out.print("Fizik Notunuzu Giriniz : ");
+    fiz = input.nextInt();
+
+    System.out.print("Kimya Notunuzu Giriniz : ");
+    kim = input.nextInt();
+
+    System.out.print("Biyoloji Notunuzu Giriniz : ");
+    biyo = input.nextInt();
+
+    System.out.print("Tarih Notunuzu Giriniz : ");
+    tarih = input.nextInt();
+
+    System.out.print("Türkçe Notunuzu Giriniz : ");
+    turkce = input.nextInt();
+
+    System.out.print("Müzik Notunuzu Giriniz : ");
+    muzik = input.nextInt();
+
+      double ortalama = (mat+ fiz+ kim+ biyo+ tarih+ turkce+ muzik)/6;
+      boolean gectimi = (ortalama>=60);
+      String durum =gectimi?" Sınıfı Geçti":" Sınıfta Kaldı";
+      String sonuc = ("Ortalamanız: "+ ortalama + durum );
+    System.out.println(sonuc);
+}
+}

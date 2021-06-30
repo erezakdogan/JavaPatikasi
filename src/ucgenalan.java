de0128066ae1a8d5af81069f70055dfe9fb08ae1
Erez <erezakdogan.ea@gmail.com>
2021-06-30 17:58:43 +0300

    ucgenalan added

---
+++ src/ucgenalan.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.Scanner;
+
+public class ucgenalan {
+    public static void main(String[] args) {
+        int a,b,c;
+        double alan;
+        Scanner kenar = new Scanner(System.in);
+        System.out.print("1.Kenarı giriniz: ");
+        a = kenar.nextInt();
+        System.out.print("2.Kenarı giriniz: ");
+        b = kenar.nextInt();
+        System.out.print("3.Kenarı giriniz: ");
+        c = kenar.nextInt();
+
+        int u = (a+b+c)/2;
+        alan = (u*(u-a)  *(u-b)*(u-c));
+        System.out.print(Math.sqrt(alan));
+
+    }
+}

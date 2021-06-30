7736660cfa5d61009e37a344ae4a626a3c87029c
Erez <erezakdogan.ea@gmail.com>
2021-06-30 17:39:28 +0300

    kdvtutari added

---
+++ src/kdvtutari.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.Scanner;
+
+public class kdvtutari {
+    public static void main(String[] args) {
+        double paraDergeri;
+        double kdvliDeger;
+        double kdvOrani = 0.18, indKdv = 0.08;
+        
+        boolean bindenBuyuk ;
+        Scanner inp = new Scanner(System.in);
+    
+        System.out.println("Para değeri giriniz: ");
+        paraDergeri = inp.nextInt();
+        bindenBuyuk=(paraDergeri>1000);
+        
+        kdvliDeger = (bindenBuyuk?paraDergeri*indKdv+paraDergeri:paraDergeri*kdvOrani+paraDergeri12); 
+        System.out.println(kdvliDeger);
+        
+    }
+}

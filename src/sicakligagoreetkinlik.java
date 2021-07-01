4e3b094cec6490bf8312f6ca68a0aa213cf9bec0
Erez <erezakdogan.ea@gmail.com>
2021-07-01 13:26:57 +0300

    sicaklıgagoreetkinlik

---
+++ src/sicakligagoreetkinlik.java
@@ -0,0 +1,30 @@
+package src;
+
+import java.util.Scanner;
+
+
+public class sicakligagoreetkinlik {
+    public static void main(String[] args) {
+        int heat;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Sıcaklık giriniz : ");
+        heat = scan.nextInt();
+        scan.close();
+        if(heat<5){
+            System.out.println("Kayak yapabilirsiniz");
+
+        } else if(5<=heat && heat <=25){
+          
+            if(heat<=15){
+            System.out.println("Sinemaya gidebilirsiniz");
+            }
+             if(10<=heat){
+            System.out.println("Pikniğe gidebilirsiniz");
+         }
+        }
+            else if(25<heat){
+            System.out.print("Yüzmeye gidebilirsiniz");
+        }
+    }
+}

026179fc367aceeba506b52c17111c7b458a977b
Erez <erezakdogan.ea@gmail.com>
2021-07-01 17:42:36 +0300

    artikyil added

---
+++ src/artikyil.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.util.Scanner;
+
+public class artikyil{
+public static void main(String[] args) {
+    int year;
+    final boolean artikyil;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Bir yıl giriniz : ");
+    year = scan.nextInt();
+
+    if(year%100 == 0){
+        artikyil = (year%400 == 0);
+        System.out.print(artikyil?year+" bir artık yıldır !" : year+" bir artık yıldır değildir !");
+    } else if(year%4 == 0){
+        System.out.print(year+" bir artık yıldır !" );
+    } else{
+        System.out.print( year+" bir artık yıldır değildir !");
+
+    }
+}
+}

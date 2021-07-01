8ed8b82a79f28457d0076b8c729906f65bd2dec2
Erez <erezakdogan.ea@gmail.com>
2021-07-02 00:02:33 +0300

    kuvvetyazdirma added

---
+++ src/kuvvetyazdirma.java
@@ -0,0 +1,29 @@
+package src;
+
+import java.util.Scanner;
+
+public class kuvvetyazdirma {
+   public static void main(String[] args) {
+       int num;
+       Scanner scanner =new Scanner(System.in);
+       System.out.print("Sayı giriniz : ");
+       num = scanner.nextInt();
+
+
+       System.out.println("Dördün Katları");
+for(int i = 1; i<=num; i*=4){
+        System.out.println(i);
+}
+
+    System.out.println("Beşin Katları");
+for(int i = 1; i<=num; i*=5){
+    System.out.println(i);
+
+   
+   
+
+        }        
+    }
+                      
+  }
+   

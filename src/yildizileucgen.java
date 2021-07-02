eac28ca0267f44ac10eef8739d2c7d7badafb925
Erez <erezakdogan.ea@gmail.com>
2021-07-02 12:22:59 +0300

    yildizileucgen added

---
+++ src/yildizileucgen.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.util.Scanner;
+
+
+public class yildizileucgen {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("N sayısını giriniz :");
+        int n = input.nextInt();
+        input.close();
+
+        for(int i=0; i<=n ; i++){
+            for(int j=0; j < (n-i); j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=(2*i+1); k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        
+        for(int i=1; i<=n ; i++){
+            for(int j=0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int k=1; k<=2*(n-i)+1; k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}

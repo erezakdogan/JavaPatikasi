a1d0fbc1a06594e0b9cd68e84696541baa1bad70
Erez <erezakdogan.ea@gmail.com>
2021-07-02 18:01:28 +0300

    tersucgen  added

---
+++ src/tersucgen.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.util.Scanner;
+
+public class tersucgen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int bas =scanner.nextInt();
+        scanner.close();
+
+        for(int i = 1; i<=bas; i++){
+
+            for(int j=1; j <= i; j++){
+                System.out.print(" ");
+            }
+            for(int k = 0; k<=2*(bas-i); k++){
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+        }
+    }
+}

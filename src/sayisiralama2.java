f6dac7a51d3e122da65b6c15105c34a8972039d5
Erez <erezakdogan.ea@gmail.com>
2021-07-02 17:26:20 +0300

    sayisiralama2 added

---
+++ src/sayisiralama2.java
@@ -0,0 +1,62 @@
+package src;
+
+import java.util.Scanner;
+
+import javax.lang.model.util.ElementScanner14;
+
+public class sayisiralama2 {
+    public static void main(String[] args) {
+   
+   
+      Scanner scanner = new Scanner(System.in);
+      System.out.print("Kaç Sayı : ");
+      int  n = scanner.nextInt();
+
+   
+   int large = 0;
+   int small = 0;
+   int i = 1;
+   do{
+
+      
+      System.out.print(i+".Sayı : ");
+      int a = scanner.nextInt();
+      if( a <= small){        
+      small = a;
+      }
+      else if(a >= large){
+      large = a;
+      }
+
+            
+               
+            
+      i++;
+   }while(i<=n);
+
+   
+   System.out.println("En küçük sayı: " + small);
+   System.out.print("En büyük sayı: "+ large);
+   scanner.close();
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+
+
+}}

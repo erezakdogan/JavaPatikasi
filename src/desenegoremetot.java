0fe0a8ad86a17d09d449fe728da84e2e12ec0ecb
Erez <erezakdogan.ea@gmail.com>
2021-07-03 22:01:25 +0300

    desenegoremetot added

---
+++ src/desenegoremetot.java
@@ -0,0 +1,26 @@
+package src;
+
+public class desenegoremetot {
+    public static void main(String[] args) {
+        desen(15);
+    }
+
+    static int desen(int a){
+    int a2= a;
+       
+       while(a>0){
+            System.out.print(" desen 2 "+a);
+            a-=5;
+            while(a<=0){
+            System.out.print(" desen 1 "+a);
+         desen(a+5);
+       }
+        }
+       
+       return a;
+         
+    }
+
+
+}
+

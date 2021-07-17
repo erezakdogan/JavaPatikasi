e64f62d9fee5cf065aa679849a18ca7320e3c3e1
Erez <erezakdogan.ea@gmail.com>
2021-07-17 19:46:30 +0300

    added

--- src/freq.java
+++ src/freq.java
@@ -1,26 +1,37 @@
 package src;
 
+import java.util.Arrays;
 
 public class freq {
     public static void main(String[] args) {
         int[] serial = {10, 20, 20,10, 10, 20, 5, 20,9,77,8};
-        int[] rep = new int[serial.length];
-        
-        for(int i = 0; i<serial.length;i++){        
-            int start = 1;
+        boolean[] bol = new boolean[serial.length];
+        Arrays.fill(bol, false);
+
+
+
+
+
+
+System.out.println("Tekrar Sayıları");
+        for(int i = 0; i<serial.length;i++){ 
+            if(bol[i]==true){
+                continue;
+            }
+            int start = 0;
            
             for(int j=0; j<serial.length;j++){
                 if(serial[i]==serial[j]){
-                  rep[i] = start++;
+                  start++;
+                  bol[j]= true;
          
                 }
             }
- 
+        System.out.println(serial[i]+" sayısı "+start+" kere tekrarladı.");
         }
 
         
-        for(int k : rep){
-            System.out.print(k+" ");
-        }
+        
+        
     }
 }

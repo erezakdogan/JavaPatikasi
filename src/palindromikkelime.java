cf310af17346a760b4cd6bac39cb07f77f9f21e7
Erez <erezakdogan.ea@gmail.com>
2021-07-18 18:08:02 +0300

    added

---
+++ src/palindromikkelime.java
@@ -0,0 +1,36 @@
+package src;
+
+import java.util.Scanner;
+
+public class palindromikkelime {
+    static boolean isPalindrome(String str) {
+        int i = 0, j = str.length() - 1;
+        while (i < j) {
+            if (str.charAt(i) != str.charAt(j))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static boolean isPalindrome2(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String abba= scanner.nextLine();
+        scanner.close();
+        System.out.println(isPalindrome(abba));
+    }
+
+}

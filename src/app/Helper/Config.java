ce25a0798c2d6647fb1fdb0a750aa7edcac46e38
Erez Akdogan <erezakdogan.ea@gmail.com>
2021-11-04 20:03:24 +0300

    insertion sort created

--- src/app/Helper/Config.java
+++ src/app/Helper/Config.java
@@ -1,10 +1,7 @@
 package app.Helper;
-
 public class Config {
     public static final String PROJECT_TITLE = "Patika.Dev";
     public static final String DB_URL = "jdbc:postgresql://localhost/patika";
     public static final String DB_USER = "postgres";
     public static final String DB_PASS = "postgresql";
-
-
 }
